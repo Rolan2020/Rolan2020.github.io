08d830fedc2c9b6522a4a5e4265813436f016ad3
Rolando Tarqui Benito <62731069+Rolan2020@users.noreply.github.com>
2020-06-16 07:40:14 -0300

    "/notas-rt";

--- notas-rtb/javascript/main.js
+++ notas-rtb/javascript/main.js
@@ -34,7 +34,7 @@
         content.classList.add("animate__animated");
 
         setTimeout(function () {          //timer
-        location.href = "/index.html";
+        location.href = "/notas-rt";
       },700);
 
         return false;
